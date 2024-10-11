193b23e776d595a52762409fc4dc9dfe8e8ca000
Gergo Koteles <soyer@irl.hu>
2024-10-11 13:06:59 +0200

    create preset pages only when needed

--- src/minilab3/ml3-preset-page.c
+++ src/minilab3/ml3-preset-page.c
@@ -1,11 +1,105 @@
 #include "ml3-preset-page.h"
 
+#include "ml3-fader-page.h"
+#include "ml3-knob-page.h"
+#include "ml3-main-knob-page.h"
+#include "ml3-mod-page.h"
+#include "ml3-pad-page.h"
+#include "ml3-pitch-page.h"
+#include "ml3-shift-page.h"
+
 struct _Ml3PresetPage
 {
   ScNavigationPage parent_instance;
 };
 
 G_DEFINE_FINAL_TYPE (Ml3PresetPage, ml3_preset_page, SC_TYPE_NAVIGATION_PAGE)
+
+static uint32_t
+get_preset_offset (ScNavigationPage *self)
+{
+  // 0x08400100 -> 0x04400000
+  uint32_t offset = sc_navigation_page_get_control_id_offset (self);
+  return 0x03400000 + (offset << 16);
+}
+
+void
+on_shift_activated (ScNavigationPage *self, AdwActionRow* row)
+{
+  AdwNavigationView *view = ADW_NAVIGATION_VIEW (gtk_widget_get_ancestor (GTK_WIDGET (self), ADW_TYPE_NAVIGATION_VIEW));
+  AdwNavigationPage *page = g_object_new (ML3_TYPE_SHIFT_PAGE, "control-id-offset", get_preset_offset (self), NULL);
+  adw_navigation_view_push (view, page);
+  g_idle_add (G_SOURCE_FUNC (sc_navigation_page_load_controls_and_update_bg), page);
+}
+
+void
+on_pitch_activated (ScNavigationPage *self, AdwActionRow* row)
+{
+  AdwNavigationView *view = ADW_NAVIGATION_VIEW (gtk_widget_get_ancestor (GTK_WIDGET (self), ADW_TYPE_NAVIGATION_VIEW));
+  AdwNavigationPage *page = g_object_new (ML3_TYPE_PITCH_PAGE, "control-id-offset", get_preset_offset (self), NULL);
+  adw_navigation_view_push (view, page);
+  g_idle_add (G_SOURCE_FUNC (sc_navigation_page_load_controls_and_update_bg), page);
+}
+
+void
+on_mod_activated (ScNavigationPage *self, AdwActionRow* row)
+{
+  AdwNavigationView *view = ADW_NAVIGATION_VIEW (gtk_widget_get_ancestor (GTK_WIDGET (self), ADW_TYPE_NAVIGATION_VIEW));
+  AdwNavigationPage *page = g_object_new (ML3_TYPE_MOD_PAGE, "control-id-offset", get_preset_offset (self), NULL);
+  adw_navigation_view_push (view, page);
+  g_idle_add (G_SOURCE_FUNC (sc_navigation_page_load_controls_and_update_bg), page);
+}
+
+void
+on_main_knob_activated (ScNavigationPage *self, AdwActionRow* row)
+{
+  AdwNavigationView *view = ADW_NAVIGATION_VIEW (gtk_widget_get_ancestor (GTK_WIDGET (self), ADW_TYPE_NAVIGATION_VIEW));
+  AdwNavigationPage *page = g_object_new (ML3_TYPE_MAIN_KNOB_PAGE, "control-id-offset", get_preset_offset (self), NULL);
+  adw_navigation_view_push (view, page);
+  g_idle_add (G_SOURCE_FUNC (sc_navigation_page_load_controls_and_update_bg), page);
+}
+
+void
+on_knob_activated (ScNavigationPage *self, AdwActionRow* row)
+{
+  AdwNavigationView *view = ADW_NAVIGATION_VIEW (gtk_widget_get_ancestor (GTK_WIDGET (self), ADW_TYPE_NAVIGATION_VIEW));
+  AdwNavigationPage *page = g_object_new (ML3_TYPE_KNOB_PAGE, "control-id-offset", get_preset_offset (self), NULL);
+  adw_navigation_view_push (view, page);
+  g_idle_add (G_SOURCE_FUNC (sc_navigation_page_load_controls_and_update_bg), page);
+}
+
+void
+on_fader_activated (ScNavigationPage *self, AdwActionRow* row)
+{
+  AdwNavigationView *view = ADW_NAVIGATION_VIEW (gtk_widget_get_ancestor (GTK_WIDGET (self), ADW_TYPE_NAVIGATION_VIEW));
+  AdwNavigationPage *page = g_object_new (ML3_TYPE_FADER_PAGE, "control-id-offset", get_preset_offset (self), NULL);
+  adw_navigation_view_push (view, page);
+  g_idle_add (G_SOURCE_FUNC (sc_navigation_page_load_controls_and_update_bg), page);
+}
+
+void
+on_pad_a_activated (ScNavigationPage *self, AdwActionRow* row)
+{
+  AdwNavigationView *view = ADW_NAVIGATION_VIEW (gtk_widget_get_ancestor (GTK_WIDGET (self), ADW_TYPE_NAVIGATION_VIEW));
+  AdwNavigationPage *page = g_object_new (ML3_TYPE_PAD_PAGE,
+                                          "control-id-offset", get_preset_offset (self),
+                                          "control-cc-offset", get_preset_offset (self),
+                                          NULL);
+  adw_navigation_view_push (view, page);
+  g_idle_add (G_SOURCE_FUNC (sc_navigation_page_load_controls_and_update_bg), page);
+}
+
+void
+on_pad_b_activated (ScNavigationPage *self, AdwActionRow* row)
+{
+  AdwNavigationView *view = ADW_NAVIGATION_VIEW (gtk_widget_get_ancestor (GTK_WIDGET (self), ADW_TYPE_NAVIGATION_VIEW));
+  AdwNavigationPage *page = g_object_new (ML3_TYPE_PAD_PAGE,
+                                          "control-id-offset", get_preset_offset (self) + 0x0800,
+                                          "control-cc-offset", get_preset_offset (self) + 0x0100,
+                                          NULL);
+  adw_navigation_view_push (view, page);
+  g_idle_add (G_SOURCE_FUNC (sc_navigation_page_load_controls_and_update_bg), page);
+}
 
 static void
 ml3_preset_page_class_init (Ml3PresetPageClass *klass)
--- src/minilab3/ml3-preset-page.h
+++ src/minilab3/ml3-preset-page.h
@@ -8,4 +8,13 @@ G_BEGIN_DECLS
 
 G_DECLARE_FINAL_TYPE (Ml3PresetPage, ml3_preset_page, ML3, PRESET_PAGE, ScNavigationPage)
 
+void on_shift_activated (ScNavigationPage *self, AdwActionRow* row);
+void on_pitch_activated (ScNavigationPage *self, AdwActionRow* row);
+void on_mod_activated (ScNavigationPage *self, AdwActionRow* row);
+void on_main_knob_activated (ScNavigationPage *self, AdwActionRow* row);
+void on_knob_activated (ScNavigationPage *self, AdwActionRow* row);
+void on_fader_activated (ScNavigationPage *self, AdwActionRow* row);
+void on_pad_a_activated (ScNavigationPage *self, AdwActionRow* row);
+void on_pad_b_activated (ScNavigationPage *self, AdwActionRow* row);
+
 G_END_DECLS
