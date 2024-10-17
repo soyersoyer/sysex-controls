5b022846fae548f575826da48f619628d627599e
Gergo Koteles <soyer@irl.hu>
2024-10-17 19:32:37 +0200

    fix function parameter name typo

--- src/sc-midi.h
+++ src/sc-midi.h
@@ -81,7 +81,7 @@ int sc_midi_arturia_v3_read_control (snd_seq_t *seq, snd_seq_addr_t addr, uint8_
 int sc_midi_arturia_v3_write_control (snd_seq_t *seq, snd_seq_addr_t addr, uint32_t control_id, uint8_t val);
 int sc_midi_arturia_recall_preset (snd_seq_t *seq, snd_seq_addr_t addr, uint8_t preset_id);
 int sc_midi_arturia_store_preset (snd_seq_t *seq, snd_seq_addr_t addr, uint8_t preset_id);
-int sc_midi_arturia_read_control (snd_seq_t *seq, snd_seq_addr_t addr, uint8_t need_ack, uint32_t control_id, uint8_t *val);
+int sc_midi_arturia_read_control (snd_seq_t *seq, snd_seq_addr_t addr, uint8_t read_ack, uint32_t control_id, uint8_t *val);
 int sc_midi_arturia_write_control (snd_seq_t *seq, snd_seq_addr_t addr, uint32_t control_id, uint8_t val);
 int sc_midi_disconnect (snd_seq_t *seq, snd_seq_addr_t addr);
 int sc_midi_connect (snd_seq_t *seq, snd_seq_addr_t addr);
