eafccefba132bb7837f3779c5c187ac91d777dac
Gergo Koteles <soyer@irl.hu>
2025-01-15 16:40:59 +0100

    reconcile parameter names in Ml3Book

--- src/minilab3/ml3-book.h
+++ src/minilab3/ml3-book.h
@@ -17,11 +17,11 @@ G_DECLARE_FINAL_TYPE (Ml3Book, ml3_book, ML3, BOOK, ArBook)
 
 void ml3_book_on_presets_preset_activated (ScNavigationPage *page, ScActionRow *row);
 
-void ml3_book_on_preset_spm_activated (ScNavigationPage *self, ScActionRow* row);
-void ml3_book_on_preset_main_knob_activated (ScNavigationPage *self, ScActionRow* row);
-void ml3_book_on_preset_knob_activated (ScNavigationPage *self, ScActionRow* row);
-void ml3_book_on_preset_fader_activated (ScNavigationPage *self, ScActionRow* row);
-void ml3_book_on_preset_pad_a_activated (ScNavigationPage *self, ScActionRow* row);
-void ml3_book_on_preset_pad_b_activated (ScNavigationPage *self, ScActionRow* row);
+void ml3_book_on_preset_spm_activated (ScNavigationPage *page, ScActionRow* row);
+void ml3_book_on_preset_main_knob_activated (ScNavigationPage *page, ScActionRow* row);
+void ml3_book_on_preset_knob_activated (ScNavigationPage *page, ScActionRow* row);
+void ml3_book_on_preset_fader_activated (ScNavigationPage *page, ScActionRow* row);
+void ml3_book_on_preset_pad_a_activated (ScNavigationPage *page, ScActionRow* row);
+void ml3_book_on_preset_pad_b_activated (ScNavigationPage *page, ScActionRow* row);
 
 G_END_DECLS
