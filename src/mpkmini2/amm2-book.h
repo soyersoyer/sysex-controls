cb2784af41abadd9b9a827ad41a23123cfd631bc
Gergo Koteles <soyer@irl.hu>
2025-06-07 21:43:35 +0200

    mpkMini2: use sc_action_nav_push

--- src/mpkmini2/amm2-book.h
+++ src/mpkmini2/amm2-book.h
@@ -3,8 +3,6 @@
 #include <adwaita.h>
 
 #include "ak-book.h"
-#include "sc-navigation-page.h"
-#include "sc-action-row.h"
 
 #define AMM2_MIDI_NAME "MPKmini2"
 #define AMM2_MIDI_NAME_2 "MPK Mini Mk II"
@@ -15,12 +13,5 @@ G_BEGIN_DECLS
 #define AMM2_TYPE_BOOK (amm2_book_get_type ())
 
 G_DECLARE_FINAL_TYPE (Amm2Book, amm2_book, AMM2, BOOK, AkBook)
-
-void amm2_book_on_arpeggiator_activated (ScNavigationPage *self, ScActionRow* row);
-void amm2_book_on_joystick_activated (ScNavigationPage *self, ScActionRow* row);
-void amm2_book_on_keybed_activated (ScNavigationPage *self, ScActionRow* row);
-void amm2_book_on_knob_activated (ScNavigationPage *self, ScActionRow* row);
-void amm2_book_on_pad_activated (ScNavigationPage *self, ScActionRow* row);
-void amm2_book_on_pad_bank_activated (ScNavigationPage *self, ScActionRow* row);
 
 G_END_DECLS
