18af6475e89a437731b66f13866ff225415d584e
Gergo Koteles <soyer@irl.hu>
2025-06-07 23:16:15 +0200

    BeatStep Pro: use sc_action_nav_push

--- src/beatsteppro/bsp-book.h
+++ src/beatsteppro/bsp-book.h
@@ -3,8 +3,6 @@
 #include <adwaita.h>
 
 #include "ar-book.h"
-#include "sc-action-row.h"
-#include "sc-navigation-page.h"
 
 #define BSP_MIDI_NAME "Arturia BeatStep Pro"
 #define BSP_SHORT_NAME "BS Pro"
@@ -14,14 +12,5 @@ G_BEGIN_DECLS
 #define BSP_TYPE_BOOK (bsp_book_get_type ())
 
 G_DECLARE_FINAL_TYPE (BspBook, bsp_book, BSP, BOOK, ArBook)
-
-void bsp_book_on_presets_preset_activated (ScNavigationPage *page, ScActionRow *row);
-
-void bsp_book_on_project_settings_activated (ScNavigationPage *page, ScActionRow* row);
-void bsp_book_on_project_knobs_activated (ScNavigationPage *page, ScActionRow* row);
-void bsp_book_on_project_steps_activated (ScNavigationPage *page, ScActionRow* row);
-void bsp_book_on_project_pads_activated (ScNavigationPage *page, ScActionRow* row);
-void bsp_book_on_project_sequencer_activated (ScNavigationPage *page, ScActionRow* row);
-void bsp_book_on_project_drum_activated (ScNavigationPage *page, ScActionRow* row);
 
 G_END_DECLS
