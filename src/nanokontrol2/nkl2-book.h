4fa2122768608799be60686e396475b10cb3abe4
Gergo Koteles <soyer@irl.hu>
2025-06-07 21:30:57 +0200

    nanoKONTROL2: use sc_action_nav_push

--- src/nanokontrol2/nkl2-book.h
+++ src/nanokontrol2/nkl2-book.h
@@ -3,8 +3,6 @@
 #include <adwaita.h>
 
 #include "korg-book.h"
-#include "sc-navigation-page.h"
-#include "sc-action-row.h"
 
 #define NKL2_MIDI_NAME "nanoKONTROL2"
 #define NKL2_SHORT_NAME "nanoKONTROL2"
@@ -14,13 +12,5 @@ G_BEGIN_DECLS
 #define NKL2_TYPE_BOOK (nkl2_book_get_type ())
 
 G_DECLARE_FINAL_TYPE (Nkl2Book, nkl2_book, NKL2, BOOK, KorgBook)
-
-void nkl2_book_on_button_activated (ScNavigationPage *page, ScActionRow* row);
-void nkl2_book_on_controller_group_activated (ScNavigationPage *page, ScActionRow* row);
-void nkl2_book_on_controller_groups_activated (ScNavigationPage *page, ScActionRow* row);
-void nkl2_book_on_function_button_activated (ScNavigationPage *page, ScActionRow* row);
-void nkl2_book_on_knob_activated (ScNavigationPage *page, ScActionRow* row);
-void nkl2_book_on_slider_activated (ScNavigationPage *page, ScActionRow* row);
-void nkl2_book_on_transport_buttons_activated (ScNavigationPage *page, ScActionRow* row);
 
 G_END_DECLS
