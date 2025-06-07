7577972dcb4448d05e13e0be6958bfc9730544be
Gergo Koteles <soyer@irl.hu>
2025-06-07 21:39:09 +0200

    mpkMini3: use sc_action_nav_push

--- src/mpkmini3/amm3-book.h
+++ src/mpkmini3/amm3-book.h
@@ -3,8 +3,6 @@
 #include <adwaita.h>
 
 #include "ak-book.h"
-#include "sc-navigation-page.h"
-#include "sc-action-row.h"
 
 #define AMM3_MIDI_NAME "MPK mini 3"
 #define AMM3_SHORT_NAME "MPK mini 3"
@@ -14,12 +12,5 @@ G_BEGIN_DECLS
 #define AMM3_TYPE_BOOK (amm3_book_get_type ())
 
 G_DECLARE_FINAL_TYPE (Amm3Book, amm3_book, AMM3, BOOK, AkBook)
-
-void amm3_book_on_arpeggiator_activated (ScNavigationPage *self, ScActionRow* row);
-void amm3_book_on_joystick_activated (ScNavigationPage *self, ScActionRow* row);
-void amm3_book_on_keybed_activated (ScNavigationPage *self, ScActionRow* row);
-void amm3_book_on_knob_activated (ScNavigationPage *self, ScActionRow* row);
-void amm3_book_on_pad_activated (ScNavigationPage *self, ScActionRow* row);
-void amm3_book_on_pad_bank_activated (ScNavigationPage *self, ScActionRow* row);
 
 G_END_DECLS
