311143f39e031704cdaed37af4b835021b989f0f
Gergo Koteles <soyer@irl.hu>
2025-06-07 22:38:57 +0200

    KeyLab Essential: use sc_action_nav_push

--- src/keylabes/kles-book.h
+++ src/keylabes/kles-book.h
@@ -3,8 +3,6 @@
 #include <adwaita.h>
 
 #include "ar-book.h"
-#include "sc-action-row.h"
-#include "sc-navigation-page.h"
 
 #define KLES_49_MIDI_NAME "Arturia KeyLab Essential 49"
 #define KLES_49_SHORT_NAME "KeyLab ES 49"
@@ -20,14 +18,5 @@ G_BEGIN_DECLS
 #define KLES_TYPE_BOOK (kles_book_get_type ())
 
 G_DECLARE_FINAL_TYPE (KlesBook, kles_book, KLES, BOOK, ArBook)
-
-void kles_book_on_presets_preset_activated (ScNavigationPage *page, ScActionRow *row);
-
-void kles_book_on_preset_keyboard_activated (ScNavigationPage *page, ScActionRow* row);
-void kles_book_on_preset_pedal_activated (ScNavigationPage *page, ScActionRow* row);
-void kles_book_on_preset_pad_activated (ScNavigationPage *page, ScActionRow* row);
-void kles_book_on_preset_button_activated (ScNavigationPage *page, ScActionRow* row);
-void kles_book_on_preset_knob_activated (ScNavigationPage *page, ScActionRow* row);
-void kles_book_on_preset_fader_activated (ScNavigationPage *page, ScActionRow* row);
 
 G_END_DECLS
